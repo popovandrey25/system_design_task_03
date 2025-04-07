5eb9d26d195d31fac048c12b9344642ac4b810d8
andrey <palanelarty@gmail.com>
2025-04-07 15:41:23 +0300

    Добавил функции для валидации пароля

--- user_service/app/auth.py
+++ user_service/app/auth.py
@@ -1,12 +1,24 @@
 import time
 import jwt
+
 from fastapi import HTTPException, Depends
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from passlib.context import CryptContext
 
 JWT_SECRET = "MY_SUPER_SECRET"
 JWT_ALGORITHM = "HS256"
 
 security = HTTPBearer()
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
 
 
 def create_access_token(username: str, expires_in: int = 3600) -> str:
